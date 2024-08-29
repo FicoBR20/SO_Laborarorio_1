4624ea3ba698e03df1bbb2734b8ad8136d4077ac
fico <federico.barbetti@correounivalle.edu.co>
2024-08-29 16:07:39 -0500

    se actualiza bisiesto.cpp

---
+++ bisiesto_year.cpp
@@ -0,0 +1,37 @@
+/*
+Archivo: bisiesto_year.cpp
+Autor: federico.barbeetti@correounivalle.edu.co>
+Fecha creación: 2024-08-29
+Fecha última modificación: 2024-08-29
+Licencia: GNU-GPL
+*/
+
+#include<iostream>
+#include <string>
+using namespace std;
+
+/*
+Este programa le solicita al usuario el ingreso del año de
+su nacimiento y le informa si nació en un año bisiesto
+*/
+
+int main(){
+    string  year;
+    int answer;
+    cout <<"Ingrese el año de su nacimiento\n";
+    getline(cin, year);
+    answer=stoi(year);
+
+    if (answer%4==0)
+        {
+            cout<<"Usted nació en un año Biciesto\n";
+        }
+
+    else{
+
+        cout<<"Usted NO nació en un año Biciesto\n";
+
+        }
+
+    return 0;
+}
